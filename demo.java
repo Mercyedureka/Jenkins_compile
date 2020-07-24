822f7e210d3dadc68cba7dea8300b4368f82fa75
edureka <mercydavid373@yahoo.com>
2020-07-24 19:17:48 +0530

    The first commit on demo.java.

---
+++ demo.java
@@ -0,0 +1,15 @@
+echo  hello
+Hello again
+ * Hello world!
+ * Hello
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	//Comment
+        System.out.println( "Hello World!" );
+    }
+}
+
+
